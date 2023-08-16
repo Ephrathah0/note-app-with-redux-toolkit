0c38239d14220e5cae232cf09a764893f36b58e4
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2023-08-16 10:31:09 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NotesList from "./features/note/NotesList";
+import AddNote from "./features/note/AddNote";
+
+function App() {
+  return (
+    <div className="App">
+      <AddNote />
+      <NotesList />
+    </div>
+  );
+}
+
+export default App;
